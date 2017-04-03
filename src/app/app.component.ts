acf00dc42a3865bb5867d47aa735b1a353f5dbc9
Mario Avila <avilamario16@gmail.com>
2017-04-02 21:31:38 -0500

    Replicat XOR exmaple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
 import { Component } from '@angular/core';
 
+declare var synaptic: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+  perceptron = null;
+  outputs = [];
+  results:any;
+
+  constructor() {
+  }
+
+  train(){
+    this.perceptron = new synaptic.Architect.Perceptron(2,3,1);
+    this.results = this.perceptron.trainer.XOR({
+  			iterations: 100000,
+  			error: .0001,
+  			rate: 1
+  		});
+      this.validate();
+    }
+
+    validate(){
+      this.outputs = [];
+  		this.outputs.push({
+  			input: '0 0',
+  			output: this.perceptron.activate([0,0])[0].toFixed(3),
+  			target: 0
+  		});
+  		this.outputs.push({
+  			input: '0 1',
+  			output: this.perceptron.activate([0,1])[0].toFixed(3),
+  			target: 1
+  		});
+  		this.outputs.push({
+  			input: '1 0',
+  			output: this.perceptron.activate([1,0])[0].toFixed(3),
+  			target: 1
+  		});
+      this.outputs.push({
+  			input: '1 1',
+  			output: this.perceptron.activate([1,1])[0].toFixed(3),
+  			target: 0
+  		});
+      console.log(this.outputs);
+  	}
 }
